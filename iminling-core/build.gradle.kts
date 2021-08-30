160f68b262eacdfac7b86ef1003ab1f2b90f253c
konghang <yslao@outlook.com>
2021-08-31 07:58:55 +0800

    jar包依赖调整

--- iminling-core/build.gradle.kts
+++ iminling-core/build.gradle.kts
@@ -16,19 +16,17 @@ repositories {
 }
 
 dependencies {
-    implementation(platform("org.springframework.boot:spring-boot-dependencies:2.3.5.RELEASE"))
-    implementation(kotlin("stdlib"))
-    implementation(project(":iminling-common"))
+    api (project(":iminling-common"))
     compileOnly ("org.projectlombok:lombok:1.16.20")
     annotationProcessor ("org.projectlombok:lombok:1.16.20")
     testCompileOnly ("org.projectlombok:lombok:1.16.20")
     testAnnotationProcessor ("org.projectlombok:lombok:1.16.20")
-    implementation ("org.springframework.boot:spring-boot-starter-web")
-    implementation ("org.springframework.boot:spring-boot-starter-actuator")
-    implementation ("com.baomidou:mybatis-plus-boot-starter:3.4.0")
-    implementation ("org.hibernate.validator:hibernate-validator:6.1.6.Final")
-    implementation ("org.aspectj:aspectjweaver:1.9.0")
-    implementation ("cglib:cglib:3.1")
-    implementation ("com.auth0:java-jwt:3.3.0")
+    api ("org.springframework.boot:spring-boot-starter-web")
+    api ("org.springframework.boot:spring-boot-starter-actuator")
+    api ("com.baomidou:mybatis-plus-boot-starter:3.4.0")
+    api ("org.hibernate.validator:hibernate-validator:6.1.6.Final")
+    api ("org.aspectj:aspectjweaver:1.9.0")
+    api ("cglib:cglib:3.1")
+    api ("com.auth0:java-jwt:3.3.0")
     testImplementation ("org.springframework.boot:spring-boot-starter-test")
 }
