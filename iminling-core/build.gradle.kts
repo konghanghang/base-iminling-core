218926c2b35d46b8ea21e17eac3246f57accfa0d
konghang <yslao@outlook.com>
2021-09-10 08:50:19 +0800

    依赖修改

--- iminling-core/build.gradle.kts
+++ iminling-core/build.gradle.kts
@@ -1,8 +1,8 @@
 dependencies {
     api(project(":iminling-common"))
+    compileOnly("com.baomidou:mybatis-plus-boot-starter:3.4.0")
     api("org.springframework.boot:spring-boot-starter-web")
     api("org.springframework.boot:spring-boot-starter-actuator")
-    api("com.baomidou:mybatis-plus-boot-starter:3.4.0")
     api("org.hibernate.validator:hibernate-validator:6.1.6.Final")
     api("org.aspectj:aspectjweaver:1.9.0")
     api("cglib:cglib:3.1")
