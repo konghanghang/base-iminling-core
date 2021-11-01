e8c60f2c160b0912399affa7ba9598facf9edf98
konghang <yslao@outlook.com>
2021-11-01 20:24:34 +0800

    date工具类优化

--- iminling-common/src/main/java/com/iminling/common/date/DateUtils.java
+++ iminling-common/src/main/java/com/iminling/common/date/DateUtils.java
@@ -4,34 +4,42 @@ import java.time.*;
 import java.time.format.DateTimeFormatter;
 
 /**
- * 日期工具类
- * java8 api: https://blog.csdn.net/chenxun_2010/article/details/72539981
+ * 日期工具类 java8 api: https://blog.csdn.net/chenxun_2010/article/details/72539981
  */
 public class DateUtils {
 
+    /**
+     * 默认完整日期-时间格式
+     */
     public static final String DEFAULT_DATE_TIME_FORMAT = "yyyy-MM-dd HH:mm:ss";
-    /** 默认日期格式 */
+    /**
+     * 默认日期格式
+     */
     public static final String DEFAULT_DATE_FORMAT = "yyyy-MM-dd";
-    /** 默认时间格式 */
+    /**
+     * 默认时间格式
+     */
     public static final String DEFAULT_TIME_FORMAT = "HH:mm:ss";
 
-    public static final int minTimestamp = 0; //最小的时间戳
-    public static final int maxTimestamp = 1999999999;//最大的时间戳
-    public static final int secondOfHour = 3600;
-    public static final int secondOfDay = 86400;//一天的秒数
-    public static final int secondOfMonth = 2592000; //一个月这里按固定30天算
-    public static final int offset = 0;
+    public static final int SECOND_MINI = 0; //最小的时间戳
+    public static final int SECOND_OF_MINUTE = 60;
+    public static final int SECOND_OF_HOUR = 60 * SECOND_OF_MINUTE;
+    public static final int SECOND_OF_DAY = 24 * SECOND_OF_HOUR;//一天的秒数
+    public static final int SECOND_OF_MONTH = 30 * SECOND_OF_DAY; //一个月这里按固定30天算
+    public static final int SECOND_OF_YEAR = 12 * SECOND_OF_MONTH;//一年的秒数
 
     /**
      * 返回unix时间戳 (1970年至今的秒数)
+     *
      * @return 毫秒数字
      */
-    public static Long getUnixStamp(){
-        return System.currentTimeMillis() /1000;
+    public static Long getUnixStamp() {
+        return System.currentTimeMillis() / 1000;
     }
 
     /**
      * 得到昨天的日期
+     *
      * @return yyyy-MM-dd
      */
     public static String getYesterdayDate() {
@@ -42,20 +50,22 @@ public class DateUtils {
 
     /**
      * 时间戳转化为时间格式
+     *
      * @param timeStamp 纳秒数字
      * @return yyyy-MM-dd
      */
     public static String timeStampToStr(long timeStamp) {
-        return timeStampToStr(timeStamp,"yyyy-MM-dd");
+        return timeStampToStr(timeStamp, DEFAULT_DATE_FORMAT);
     }
 
     /**
      * 时间戳转对应格式
+     *
      * @param timeStamp 时间戳
-     * @param format 时间格式
+     * @param format    时间格式
      * @return 对应时间格式
      */
-    public static String timeStampToStr(long timeStamp,String format){
+    public static String timeStampToStr(long timeStamp, String format) {
         Instant instant = Instant.ofEpochSecond(timeStamp);
         LocalDateTime localDateTime = LocalDateTime.ofInstant(instant, ZoneId.systemDefault());
         return localDateTime.format(DateTimeFormatter.ofPattern(format));
@@ -63,7 +73,8 @@ public class DateUtils {
 
     /**
      * 得到时间  HH:mm:ss
-     * @param timeStamp   时间戳
+     *
+     * @param timeStamp 时间戳
      * @return HH:mm:ss
      */
     public static String getTime(long timeStamp) {
@@ -82,19 +93,18 @@ public class DateUtils {
     public static String convertTimeToFormat(long timeStamp) {
         long curTime = getUnixStamp();
         long time = curTime - timeStamp;
-
-        if (time < 60 && time >= minTimestamp) {
+        if (time < SECOND_OF_MINUTE && time >= SECOND_MINI) {
             return "刚刚";
-        } else if (time >= 60 && time < secondOfHour) {
-            return time / 60 + "分钟前";
-        } else if (time >= secondOfHour && time < secondOfDay) {
-            return time / secondOfHour + "小时前";
-        } else if (time >= secondOfDay && time < secondOfMonth) {
-            return time / secondOfHour / 24 + "天前";
-        } else if (time >= secondOfMonth && time < secondOfHour * 24 * 30 * 12) {
-            return time / secondOfHour / 24 / 30 + "个月前";
-        } else if (time >= secondOfHour * 24 * 30 * 12) {
-            return time / secondOfHour / 24 / 30 / 12 + "年前";
+        } else if (time >= SECOND_OF_MINUTE && time < SECOND_OF_HOUR) {
+            return time / SECOND_OF_MINUTE + "分钟前";
+        } else if (time >= SECOND_OF_HOUR && time < SECOND_OF_DAY) {
+            return time / SECOND_OF_HOUR + "小时前";
+        } else if (time >= SECOND_OF_DAY && time < SECOND_OF_MONTH) {
+            return time / SECOND_OF_DAY + "天前";
+        } else if (time >= SECOND_OF_MONTH && time < SECOND_OF_YEAR) {
+            return time / SECOND_OF_MONTH + "个月前";
+        } else if (time >= SECOND_OF_YEAR) {
+            return time / SECOND_OF_YEAR + "年前";
         } else {
             return "刚刚";
         }
@@ -102,27 +112,29 @@ public class DateUtils {
 
     /**
      * 获取当天00:00:00时间戳
-     * @return {@link Long}
+     *
+     * @return {@link Long} 单位秒
      */
     public static Long getDayStartTime() {
         LocalDateTime localDateTime = LocalDateTime.now()
-                .withHour(0)
-                .withMinute(0)
-                .withSecond(0);
+            .withHour(0)
+            .withMinute(0)
+            .withSecond(0);
         return localDateTime.atZone(ZoneId.systemDefault()).toInstant().getEpochSecond();
     }
 
     /**
      * 计算两个时间戳之间相隔的天数
-     * @param begin 开始时间 纳秒
-     * @param end 结束时间 纳秒
+     *
+     * @param begin 开始时间 秒
+     * @param end   结束时间 秒
      * @return 相差天数
      */
-    public static int getIntervalDays(Long begin, Long end){
+    public static int getIntervalDays(Long begin, Long end) {
         LocalDateTime beginDate = LocalDateTime.ofInstant(Instant.ofEpochSecond(begin), ZoneId.systemDefault());
         LocalDateTime endDate = LocalDateTime.ofInstant(Instant.ofEpochSecond(end), ZoneId.systemDefault());
         Duration duration = Duration.between(beginDate, endDate);
-        return Integer.valueOf(duration.toDays()+"");
+        return Long.valueOf(duration.toDays()).intValue();
     }
 
 }
---
+++ iminling-common/src/test/java/com/iminling/common/date/DateUtilsTest.java
@@ -0,0 +1,40 @@
+package com.iminling.common.date;
+
+import org.junit.jupiter.api.Test;
+
+class DateUtilsTest {
+
+    @Test
+    void getUnixStamp() {
+    }
+
+    @Test
+    void getYesterdayDate() {
+        String yesterdayDate = DateUtils.getYesterdayDate();
+        System.out.println(yesterdayDate);
+    }
+
+    @Test
+    void timeStampToStr() {
+    }
+
+    @Test
+    void testTimeStampToStr() {
+    }
+
+    @Test
+    void getTime() {
+    }
+
+    @Test
+    void convertTimeToFormat() {
+    }
+
+    @Test
+    void getDayStartTime() {
+    }
+
+    @Test
+    void getIntervalDays() {
+    }
+}
