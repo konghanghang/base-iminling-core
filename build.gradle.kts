b467e40490d51a2ef6364742166e103f858287e7
konghang <yslao@outlook.com>
2022-01-25 20:10:45 +0800

    2.2.2版本

--- build.gradle.kts
+++ build.gradle.kts
@@ -67,7 +67,7 @@ subprojects {
     }
 
     group = "com.iminling"
-    version = "2.2.2-SNAPSHOT"
+    version = "2.2.2"
 
     dependencies {
         api(platform("org.springframework.boot:spring-boot-dependencies:2.3.5.RELEASE"))
